3dd9f22a5c580dae1dadf2834b31e9cdedce7e85
lucasmoraist <luksmnt1101@gmail.com>
2025-06-20 12:46:34 -0300

    feat: add logo image to HeaderComponent and update icon alt texts

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,25 +1,28 @@
 export function HeaderComponent() {
     // TODO: Adicionar rotas para os ícones de pesquisa, caixa de entrada e usuário, após criar as páginas correspondentes.
     return (
-        <header className="flex justify-between items-center p-4 border-b border-stone-300">
-            {/* Adicionar a logo aqui */}
-            <img src="" alt="" />
+        <header className="flex flex-col sm:flex-row justify-between items-center gap-4 p-4 border-b border-stone-300">
+            <img
+                src="assets/image/collabhubbr.png"
+                alt="Logo CollabHubBR"
+                className="w-40 sm:w-48 md:w-52"
+            />
 
             <div className="flex gap-4">
                 <img
                     src="assets/svg/search.svg"
-                    alt=""
-                    className="w-7 cursor-pointer"
+                    alt="Search Icon"
+                    className="w-6 sm:w-7 cursor-pointer"
                 />
                 <img
                     src="assets/svg/inbox.svg"
-                    alt=""
-                    className="w-7 cursor-pointer"
+                    alt="Inbox Icon"
+                    className="w-6 sm:w-7 cursor-pointer"
                 />
                 <img
                     src="assets/svg/user.svg"
-                    alt=""
-                    className="w-7 cursor-pointer"
+                    alt="User Icon"
+                    className="w-6 sm:w-7 cursor-pointer"
                 />
             </div>
         </header>
