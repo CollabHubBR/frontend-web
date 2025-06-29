d1a2e2d6c3a3f72ea46a08ad1f9ebac77c2635ec
lucasmoraist <luksmnt1101@gmail.com>
2025-06-28 23:23:11 -0300

    style: update input text color in SearchComponent for better visibility

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -4,7 +4,7 @@ export function SearchComponent() {
       <input
         type="text"
         placeholder="Encontre um serviço"
-        className="flex-grow px-4 py-2 bg-slate-950 text-white placeholder-gray-400 focus:outline-none"
+        className="flex-grow px-4 py-2 bg-slate-950 text-slate-100 placeholder-gray-400 focus:outline-none"
       />
       <button className="bg-sky-600 px-4 flex items-center justify-center cursor-pointer">
         <img src="assets/svg/search.svg" alt="Buscar" className="w-5 h-5" />
